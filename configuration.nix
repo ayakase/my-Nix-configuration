bd316b6e2273bd2b354a82abfe0d5902b3e87ba8
Đặng Thái An <wibu1892001@gmail.com>
2025-01-08 23:54:28 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,63 +1,134 @@
-# /etc/nixos/configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
-  # Basic system configuration
-  imports = [ ./hardware-configuration.nix ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  # Define system attributes
-  system.stateVersion = "23.05"; # Adjust to your installed NixOS version
-  boot.loader.grub.device = "/dev/sda"; # Replace with your disk
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.hostName = "my-nixos-system";
-  networking.useDHCP = true;
+  networking.networkmanager.enable = true;
 
-  # Enable basic services
-  services.openssh.enable = true; # SSH for remote access
+  # Set your time zone.
+  time.timeZone = "Asia/Ho_Chi_Minh";
 
-  # Install system packages
-  environment.systemPackages = with pkgs; [
-    nodejs-20_x  # Node.js 20
-    microsoft-edge-stable # Microsoft Edge
-    redis          # Redis CLI
-    postgresql     # PostgreSQL CLI tools
-  ];
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  # PostgreSQL configuration
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_15; # Adjust version as needed
-    authentication = pkgs.lib.mkOverride 10 ''
-      local   all             all                                     trust
-      host    all             all             127.0.0.1/32            md5
-      host    all             all             ::1/128                 md5
-    '';
-    initialScript = ''
-      CREATE USER myuser WITH PASSWORD 'mypassword';
-      CREATE DATABASE mydb OWNER myuser;
-    '';
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "vi_VN";
+    LC_IDENTIFICATION = "vi_VN";
+    LC_MEASUREMENT = "vi_VN";
+    LC_MONETARY = "vi_VN";
+    LC_NAME = "vi_VN";
+    LC_NUMERIC = "vi_VN";
+    LC_PAPER = "vi_VN";
+    LC_TELEPHONE = "vi_VN";
+    LC_TIME = "vi_VN";
   };
 
-  # Redis configuration
-  services.redis = {
-    enable = true;
-    bind = "127.0.0.1"; # Bind to localhost for security
-    port = 6379;        # Default Redis port
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
   };
 
-  # Enable Flatpak for Edge if necessary (optional)
-  programs.flatpak.enable = true;
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-  # Security and firewall settings
-  networking.firewall.allowedTCPPorts = [ 5432 6379 ]; # PostgreSQL and Redis ports
-  networking.firewall.enable = true;
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
-  # Enable Microsoft Edge via Flatpak
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ayakase = {
+    isNormalUser = true;
+    description = "ayakase";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    microsoft-edge-stable
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  vscode
+  microsoft-edge
+  ranger
+  neofetch
   ];
 
-  # Optional: Enable Nix daemon for multi-user setups
-  services.nix-daemon.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
 }
